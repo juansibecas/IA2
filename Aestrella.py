3cc3518f1f91b4e8ca4954cc6be7329f4b9d31d1
Facundo Cárdenas <facu26giol@gmail.com>
2021-04-14 20:16:46 -0300

    avance

---
+++ Aestrella.py
@@ -0,0 +1,5 @@
+def heuristica(actual, final): #función heuristica
+    h=0
+    for x in range(len(actual)):
+        h+=(actual[x]-final[x])**2
+    return math.sqrt(h)
--- TP1_2.py
+++ TP1_2.py
@@ -1,2 +1,7 @@
-import random
-import math
+from Almacen import *
+from Aestrella import *
+
+
+if __name__ == '__main__':
+    columnas = 10
+    filas = 11
