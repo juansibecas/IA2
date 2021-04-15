43414d6195cdd5e0223471e46c9793de76cafda2
Facundo Cárdenas <facu26giol@gmail.com>
2021-04-15 00:51:28 -0300

    avance

--- TP1_2.py
+++ TP1_2.py
@@ -1,7 +1,11 @@
-from Almacen import *
-from Aestrella import *
+from Almacen import Almacen
+#from Aestrella import *
 
 
 if __name__ == '__main__':
     columnas = 10
     filas = 11
+    almacen = Almacen(columnas,filas)
+    mapa = almacen.crear_almacen()
+    pasillo=almacen.crear_pasillo()
+    estante=almacen.crear_estante()
