ca9242a4be234593064158f5777bdef412863c3e
Facundo Cárdenas <facu26giol@gmail.com>
2021-05-04 16:31:44 -0300

    creacion de clases

--- TP1_4/main.py
+++ TP1_4/main.py
@@ -4,16 +4,27 @@ from Annealing import Annealing
 import random
 
 def orders():
+    line=[]
     orders=open("orders.txt")
-    line=orders.readline()
-    print(line)
+    line.append(orders.readline())
+    i=0
+    while line[i] != '':
+        i+=1
+        line.append(orders.readline())
 
+def individuals(shelves):
+    
+    n=len(shelves)
+    individual=random.sample(range(1,n+1),n)
+    print(individual)
+    
 if __name__ == "__main__":
     max_it = 1000 
     columns=13
     rows=16
     store = Almacen(rows,columns)
     map = store.almacen
-    shelve=store.crear_pasillo()
+    shelves=store.crear_pasillo()
     hal=store.crear_estante()
     orders()
+    individuals(shelves)
