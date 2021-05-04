14f9bf1544bf7ec23d156de8d3e7da82e9cf1c3b
Facundo Cárdenas <facu26giol@gmail.com>
2021-05-04 13:24:59 -0300

    creacion de clases

--- TP1_4/main.py
+++ TP1_4/main.py
@@ -1,4 +1,19 @@
+from Almacen import Almacen
+from Aestrella import Aestrella
+from Annealing import Annealing
+import random
+
+def orders():
+    orders=open("orders.txt")
+    line=orders.readline()
+    print(line)
+
 if __name__ == "__main__":
     max_it = 1000 
-    colums=13
+    columns=13
     rows=16
+    store = Almacen(rows,columns)
+    map = store.almacen
+    shelve=store.crear_pasillo()
+    hal=store.crear_estante()
+    orders()
