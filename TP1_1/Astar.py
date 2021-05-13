88608dece7d2a31c2221f0f02013926550181aca
Juan Pablo Sibecas <juan.sibecas@gmail.com>
2021-05-13 13:30:16 -0300

    Ejercicio 1, arreglados los obstaculos e implementacion de __hash__ y __eq__ en clase Node para poder eliminar duplicados. ya esta 100% terminado creo

--- TP1_1/Astar.py
+++ TP1_1/Astar.py
@@ -1,66 +1,86 @@
 import math
 from copy import copy
-from Espacio import Espacio
 
 class Astar:
-    def __init__(self, inicial, final, espacio):
-        self.inicial = inicial
-        self.final = final
-        self.espacio = espacio
+    def __init__(self, start, finish, space, it):
+        self.start = start
+        self.finish = finish
+        self.space = space
+        self.it = it
 
-    def h_euclid(self, actual, final, k): #función heuristica, calculamos la distancia euclidiana entre dos puntos.
+    def h_euclid(self, actual, finish, k): #función heuristica, calculamos la distancia euclidiana entre dos puntos.
         v = []
         for i in range(len(actual)):
-            v.append(pow((actual[i]-final[i]),2))
-        return math.sqrt(sum(v))*k 
+            v.append(pow((actual[i]-finish[i]),2))
+        return math.sqrt(sum(v))*k  #k elegido por usuario segun necesidad. factor de multiplicacion normalmente igual a 1
 
 
-    def camino(self): #Funcion para encontrar el menor camino.
-            vecinos=[]  #guardaremos todos los vecinos de la posicion actual
-            nodos=[]    #guardaremos todos los nodos - ver class Nodo
+    def path(self): #Funcion para encontrar el menor camino.
+            neighbours=[]  #guardaremos todos los vecinos de la posicion actual
+            nodes=[]    #guardaremos todos los nodos - ver class Nodo
             flag=1
             f=0
-            prev = Nodo(self.inicial, 0, 0, 0)
+            prev = Node(self.start, 0, 0, 0)
             g=0
+            it = 0
+            
+            if self.start == self.finish: #termina de una si empieza en el destino
+                return [[self.start]]
+
             while flag:
-                g+=1 #valor de la funcion g, es una unidad
-                vecinos=self.espacio.find_neighbours(prev.pos)
-                for j in range(len(vecinos)):
-                    f=g+self.h_euclid(vecinos[j], self.final, 1)      #calculamos la funcion f para cada vecino, g vale una unidad por cada movimiento
-                    nodos.append(Nodo(vecinos[j], f, g, prev))
+                it+=1
+                g+=1
+                neighbours=self.space.find_neighbours(prev.pos)
                 
-                nodos.sort(key=sort_by_f) #se ordena la lista de nodos de menor a mayor segun f
-                if nodos[0].nivel < g:
-                    g = nodos[0].nivel #si el algoritmo vuelve a una rama anterior, vuelve el valor de g al original
-                    
+                for j in range(len(neighbours)):
+                    f=g+self.h_euclid(neighbours[j], self.finish, 1)      #calculamos la funcion f para cada vecino, g vale una unidad por cada movimiento
+                    nodes.append(Node(neighbours[j], f, g, prev))
                 
-                prev = copy(nodos[0])
-                nodos.pop(0)                    #eliminamos el valor actual de los nodos para que no se pueda volver a el               
-                if self.final in vecinos:       #si el final esta dentro de los vecinos, se mueve a el y termina el programa. Llegamos
-                    prev = Nodo(self.final, 0, 0, copy(prev))    
+                
+                nodes = delete_duplicates(nodes)  #aca hay que quitar duplicados, es decir, nodos de la misma posicion y con un valor igual de f
+                
+                nodes.sort(key=sort_by_f) #se ordena la lista de nodos de menor a mayor segun f
+                
+                
+                g = nodes[0].level #si el algoritmo vuelve a una rama anterior, vuelve el valor de g al original
+                
+                prev = copy(nodes[0])
+                nodes.pop(0)                    #eliminamos el valor actual de los nodos para que no se pueda volver a el   
+                
+                if self.finish in neighbours:       #si el final esta dentro de los vecinos, se mueve a el y termina el programa. Llegamos  
                     flag=0
-            
-            camino = []
-            camino = prev.connect_path(camino) 
-            camino.reverse()
-            
-            return camino
+                    
+                if it == self.it:   
+                    flag=0
 
+            path = []
+            path = prev.connect_path(path) 
+            path.reverse()
+            
+            return path
 
+def delete_duplicates(lst):
+    return list(set(lst))
 
 def sort_by_f(nodo):
     return nodo.f
 
-class Nodo:
-    def __init__(self, pos, f, nivel, prev):  
+class Node:
+    def __init__(self, pos, f, level, prev):  
         self.pos = pos
         self.f = f
-        self.nivel = nivel #es el valor de g, los espacios recorridos hasta llegar aca
+        self.level = level #es el valor de g, los espacios recorridos hasta llegar aca
         self.prev = prev #guarda el nodo previo
+        
+    def __eq__(self, other): #eq y hash se definen para que el object sea hashable, y se pueda usar set() para eliminar duplicados
+        return self.pos == other.pos and self.f == other.f
+    
+    def __hash__(self):
+        return hash((tuple(self.pos), self.f))
     
     def connect_path(self, path): #se conecta un nodo al previo y asi sucesivamente hasta llegar al inicio del recorrido
         path.append(self.pos)
-        if self.prev == 0:
+        if self.level == 0:
             return path
         else:
             return self.prev.connect_path(path)
