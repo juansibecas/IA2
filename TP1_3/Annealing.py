9344beee073136d77a5ea01b79bce95b345dc72a
Juan Pablo Sibecas <juan.sibecas@gmail.com>
2021-05-13 14:31:37 -0300

    actualizado para los Astar y Warehouse nuevos. toque algunas cositas para que funcione con las estructuras nuevas pero nada mas

--- TP1_3/Annealing.py
+++ TP1_3/Annealing.py
@@ -1,24 +1,27 @@
 import random
 import math
-from Aestrella import Aestrella
-from Almacen import Almacen
+from Astar import Astar
+from Warehouse import Warehouse
 
 class Annealing:
-    def __init__(self,tempini,tempfin,alph,neighs,rows,columns): 
+    def __init__(self, tempini, tempfin, alph, neighbours, rows, columns, dx, dy): 
         self.tempini = tempini 
         self.tempfin = tempfin
         self.alph = alph
-        self.neighs = neighs
+        self.neighbours = neighbours
         self.rows = rows
         self.columns = columns
+        self.wh_dx = dx
+        self.wh_dy = dy
     
-    def astar_path(self,pick_in,pick_fin):              #decidi crear un almacen en cada intervalo de 
-        warehouse = Almacen(self.rows,self.columns)    #picking porque las restricciones de vecinos podian 
-        warehouse_map =  warehouse.almacen              #mamar la busqueda siguiente
-        hall = warehouse.crear_pasillo
-        shelf = warehouse.crear_estante
-        astar = Aestrella(pick_in,pick_fin,warehouse_map,hall,shelf)
-        return astar.camino(self.columns,self.rows)     #devuelve el camino de la busqueda entre dos productos
+    def astar_path(self, pick_in, pick_fin):              #decidi crear un almacen en cada intervalo de 
+        warehouse = Warehouse(self.rows, self.columns, self.wh_dx, self.wh_dy)    #picking porque las restricciones de vecinos podian 
+                                                          #mamar la busqueda siguiente
+        warehouse.create_aisles()
+        warehouse.create_shelves()
+        astar = Astar(pick_in, pick_fin, warehouse)
+
+        return astar.path()     #devuelve el camino de la busqueda entre dos productos
 
     def get_energy(self,state):            #funcion para conseguir la energia del estado, es decir, la cantidad de nodos 
         path=[]                            #que visito en total al hacer la busqueda anidada de pj. 5 productos
@@ -29,18 +32,18 @@ class Annealing:
         return len(path)
     
     def get_neighbour(self):            #toma los puntos de picking generados aleatoriamente y crea un vecino con una permutacion
-        rand_neigh= self.neighs         #cada vecino es un arreglo de puntos en el almacen, pj 5 productos 
-        val1= random.choice(rand_neigh) 
-        val2=random.choice(rand_neigh)
+        rand_neighbours = self.neighbours         #cada vecino es un arreglo de puntos en el almacen, pj 5 productos 
+        val1= random.choice(rand_neighbours) 
+        val2=random.choice(rand_neighbours)
         while val2 == val1:
-            val2= random.choice(rand_neigh)        
-        ind1 = rand_neigh.index(val1)
-        ind2 = rand_neigh.index(val2)
-        rand_neigh.remove(val1)
-        rand_neigh.insert(ind1, val2)
-        rand_neigh.remove(val2)
-        rand_neigh.insert(ind2, val1)
-        return rand_neigh             #retorna un arreglo similar al inicial pero con una permutacion en el orden de pick
+            val2= random.choice(rand_neighbours)        
+        ind1 = rand_neighbours.index(val1)
+        ind2 = rand_neighbours.index(val2)
+        rand_neighbours.remove(val1)
+        rand_neighbours.insert(ind1, val2)
+        rand_neighbours.remove(val2)
+        rand_neighbours.insert(ind2, val1)
+        return rand_neighbours           #retorna un arreglo similar al inicial pero con una permutacion en el orden de pick
 
     def simulated_annealing(self, init_state):
         current_temp= self.tempini              #temperatura inicial (alta)
--- TP1_3/TP1_3.py
+++ TP1_3/TP1_3.py
@@ -1,30 +1,38 @@
 from Annealing import Annealing
-from Almacen import Almacen
+from Warehouse import Warehouse
 import random
 
 def creat_prod(shelf, rows, columns): #func para que los puntos inicial y final no se generen en una estanteria
-    product = [random.randint(0,rows-1),random.randint(0,columns-1)]
+    product = [random.randint(0,rows-1), random.randint(0,columns-1)]
     if product in shelf: 
         return creat_prod(shelf, rows, columns)
     else:
         return product
 
 if __name__=='__main__':
-    columns = 10
-    rows = 10
-    warehouse = Almacen(rows,columns)
-    shelf = warehouse.crear_estante()
+    columns = 16  #para el genético trabajamos con 16, 13, 3, 6
+    rows = 13
+    dx = 3
+    dy = 6
+    warehouse = Warehouse(rows,columns, dx, dy)
+    warehouse.create_aisles()
+    shelves = warehouse.create_shelves()
+
     k = 5 #cantidad de productos en la lista de pick
     initial_picks = []    
    
     for i in range(k):
-        initial_picks.append(creat_prod(shelf,rows,columns))
+        initial_picks.append(creat_prod(shelves,rows,columns))
+    
     temp_ini = 100
     temp_fin = .1
-    alph = 0.01
-    annealing = Annealing(temp_ini,temp_fin,alph,initial_picks,rows,columns)
-    order = annealing.simulated_annealing(initial_picks)
-    picknumber = {n: i for i, n in enumerate(initial_picks)} 
+    alph = 0.1
+    annealing = Annealing(temp_ini, temp_fin, alph, initial_picks, rows, columns, dx, dy)
+    order_temp = annealing.simulated_annealing(initial_picks)
+    order = []
+    init = []
+        
+    picknumber = {n: i for i, n in enumerate([tuple(n) for n in initial_picks])} #con lista de tuplas funca
     result = list(map(picknumber.get, order))
     print("El orden adecuado de pick es: ")
     print(result)
