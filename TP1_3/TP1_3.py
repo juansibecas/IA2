1993c2e1cf1ca2260ebb358c5eb68881f8bce72f
Juan Pablo Sibecas <juan.sibecas@gmail.com>
2021-05-19 16:51:17 -0300

    algunos toquecitos al annealing

--- TP1_3/TP1_3.py
+++ TP1_3/TP1_3.py
@@ -1,15 +1,19 @@
 from Annealing import Annealing
 from Warehouse import Warehouse
 import random
-
+from copy import copy
+import time
+"""DEPRECATED (no hace falta que los puntos queden en pasillos)
 def creat_prod(shelf, rows, columns): #func para que los puntos inicial y final no se generen en una estanteria
     product = [random.randint(0,rows-1), random.randint(0,columns-1)]
     if product in shelf: 
         return creat_prod(shelf, rows, columns)
     else:
         return product
+"""
 
 if __name__=='__main__':
+    t1 = time.time()
     columns = 16  #para el genético trabajamos con 16, 13, 3, 6
     rows = 13
     dx = 3
@@ -18,23 +22,25 @@ if __name__=='__main__':
     warehouse.create_aisles()
     shelves = warehouse.create_shelves()
 
-    k = 5 #cantidad de productos en la lista de pick
+    k = 100 #cantidad de productos en la lista de pick
     initial_picks = []    
    
     for i in range(k):
-        initial_picks.append(creat_prod(shelves,rows,columns)) #no hace falta que uses el create_prod.
+        initial_picks.append([random.randint(0,rows-1), random.randint(0,columns-1)]) #no hace falta que uses el create_prod.
                                                                 #cambie el astar para que admita el caso de las estanterias tambien
-    print(initial_picks) 
-    temp_ini = 100
-    temp_fin = .1
-    alph = 0.1
-    annealing = Annealing(temp_ini, temp_fin, alph, initial_picks, rows, columns, dx, dy)
-    order_temp = list(map(tuple,annealing.simulated_annealing(initial_picks)))
-    order = []
-    init = []
-        
+                                                                
+    print("lugares a visitar:", initial_picks) 
+    temp_ini = 50
+    temp_fin = 0.1
+    alph = 0.99
+    annealing = Annealing(temp_ini, temp_fin, alph, initial_picks, warehouse)
+
+    order_temp, total_path_length = annealing.simulated_annealing(initial_picks)
+    order_temp = list(map(tuple,order_temp))
     picknumber = {(n,j): i for i, (n,j) in enumerate(initial_picks)} #con lista de tuplas funca
     result = list(map(picknumber.get, order_temp))
+    print("longitud total a recorrer:", total_path_length)
     print("El orden adecuado de pick es: ")
     print(result)
-
+    t2 = time.time()
+    print("tiempo de ejecucion:", t2-t1)
