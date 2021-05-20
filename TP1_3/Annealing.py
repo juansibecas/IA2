4770ffcaafd94a0806f4011c135196a8f6092e28
Ignacio Perez Allub <ignaciomartin.perezallub@gmail.com>
2021-05-20 14:18:50 -0300

    Graficas Normalizadas 0 a 1

---
+++ TP1_3/Annealing.py
@@ -0,0 +1,63 @@
+import random
+import math
+from Astar import Astar
+from Warehouse import Warehouse
+from copy import copy
+import matplotlib.pyplot as plt
+
+class Annealing:
+    def __init__(self, tempini, tempfin, alph, neighbours, warehouse): 
+        self.tempini = tempini 
+        self.tempfin = tempfin
+        self.alph = alph
+        self.neighbours = neighbours
+        self.warehouse = warehouse
+        self.energy=[]
+        self.temperature=[]
+    
+    def astar_path(self, pick_in, pick_fin):
+        astar = Astar(pick_in, pick_fin, self.warehouse)
+        self.warehouse.clear_map() #el problema estaba en como chequea los lugares que ya visito, agregue esta func para limpiar el mapa despues de cada a*
+        return astar.path()     #devuelve el camino de la busqueda entre dos productos
+
+    def get_energy(self, state):            #funcion para conseguir la energia del estado, es decir, la cantidad de nodos 
+        path=[]                            #que visito en total al hacer la busqueda anidada de pj. 5 productos
+        
+        for i in range (len(state)-1):
+            pick1=state[i]
+            pick2=state[i+1]
+            path.extend(self.astar_path(pick1,pick2))  #se van agregando los caminos entre cada pick
+        return len(path)
+    
+    def get_neighbour(self, solution):            #toma los puntos de picking generados aleatoriamente y crea un vecino con una permutacion
+        rand_neighbours = copy(solution)         #cada vecino es un arreglo de puntos en el almacen, pj 5 productos 
+        
+        idx1 = random.randint(0, len(rand_neighbours)-1)
+        idx2 = random.randint(0, len(rand_neighbours)-1)
+        
+        rand_neighbours[idx1], rand_neighbours[idx2] = rand_neighbours[idx2], rand_neighbours[idx1]
+        return rand_neighbours           #retorna un arreglo similar al inicial pero con una permutacion en el orden de pick
+
+    def simulated_annealing(self, init_state):
+        current_temp = self.tempini              #temperatura inicial (alta)
+        solution = init_state              #estado actaul: arreglo inicial de picking (producto 1, producto 2,....)
+
+        while current_temp > self.tempfin:
+            self.temperature.append(current_temp)
+            self.energy.append(self.get_energy(solution))
+            neigh = self.get_neighbour(solution) #generacion de estado vecino 
+            energy_diff = self.get_energy(solution) - self.get_energy(neigh) #diferencia de energia entre estados
+            if energy_diff > 0: #puede o no cambiar el estado actual (estocasticidad)
+                solution = neigh #si es menor lo toma
+            if random.uniform(0,1) < math.exp(energy_diff/current_temp): #si es mayor pero bajo una probabilidad, lo toma (va energy_diff sin signo, el negativo ya lo trae)
+                solution = neigh
+            current_temp *= self.alph #resta a la temp una constante (funcion de varacion lineal -- puede ser exp o log tambien)
+        #print("la solucion es:", solution)
+        total_path_length = self.get_energy(solution)
+
+        return solution, total_path_length
+    
+        
+    
+    
+   
---
+++ TP1_3/TP1_3.py
@@ -0,0 +1,76 @@
+from Annealing import Annealing
+from Warehouse import Warehouse
+import random
+from copy import copy
+import time
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+
+
+if __name__=='__main__':
+    columns = 16  #para el genético trabajamos con 16, 13, 3, 6
+    rows = 13
+    dx = 3
+    dy = 6
+    temp_ini = 100
+    temp_fin = 0.1
+    alph = 0.98
+    k = 20 #cantidad de productos en la lista de pick
+    m=20 #cantidad de ordenes
+    plt.xlim(0, 1)
+    t1 = time.time()
+        
+    warehouse = Warehouse(rows,columns, dx, dy)
+    warehouse.create_aisles()
+    shelves = warehouse.create_shelves()
+
+    scaler = preprocessing.MinMaxScaler()
+    temperature=[]
+    energy=[]
+    for i in range (m):
+
+        print(i)        
+        initial_picks = []    
+    
+        for i in range(k):
+            initial_picks.append([random.randint(0,rows-1), random.randint(0,columns-1)]) #no hace falta que uses el create_prod.
+                                                                    #cambie el astar para que admita el caso de las estanterias tambien
+                                                                    
+        #print("lugares a visitar:", initial_picks) 
+        
+        annealing = Annealing(temp_ini, temp_fin, alph, initial_picks, warehouse)
+
+        order_temp, total_path_length = annealing.simulated_annealing(initial_picks)
+        
+        temperature.append(annealing.temperature)
+        energy.append(annealing.energy)
+        
+        """
+        order_temp = list(map(tuple,order_temp))
+        picknumber = {(n,j): i for i, (n,j) in enumerate(initial_picks)} #con lista de tuplas funca
+        result = list(map(picknumber.get, order_temp))
+        print("longitud total a recorrer:", total_path_length)
+        print("El orden adecuado de pick es: ")
+        print(result)
+        """ 
+    t2 = time.time()
+    print("tiempo de ejecucion:", t2-t1)   
+           
+    n=[]
+    n=list(range(np.shape(np.transpose(np.array(temperature)))[1]))
+
+    temp_df = pd.DataFrame(np.flip(np.transpose(np.array(temperature))), columns=n)
+    scaled_temp = scaler.fit_transform(temp_df)
+    scaled_temp = pd.DataFrame(scaled_temp)
+    
+    energy_df = pd.DataFrame(np.flip(np.transpose(np.array(energy))), columns=n)
+    scaled_energy = scaler.fit_transform(energy_df)
+    scaled_energy = pd.DataFrame(scaled_energy)    
+    
+    for i in range (m):
+        plt.plot(scaled_temp.iloc[:,i],scaled_energy.iloc[:,i])
+    plt.show()
+    
