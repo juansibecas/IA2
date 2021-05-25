f3325a62d96cc90241913138013d2692303b6bbf
Juan Pablo Sibecas <juan.sibecas@gmail.com>
2021-05-25 01:30:18 -0300

    ordenado un poquito mejor

--- TP1_4/TP1_4.py
+++ TP1_4/TP1_4.py
@@ -5,19 +5,19 @@ from Orders import Order
 from Gen import Gen
 import random
 
-def orders(): #Funcion para leer el archivo de ordenes y separar cada orden
+def orders(init_pos): #Funcion para leer el archivo de ordenes y separar cada orden
     orders=[]
     lines=[]
     with open('orders.txt', 'r') as f:
         line=(f.readline())
-        orders.append(Order(line))
+        orders.append(Order(line, init_pos))
         i=0
         while line != '': #Para indicar que estamos en el fin de la lista   
             if line == '\n': #Si solo tenemos \n pasamos a la siguiente orden
                 orders[i].setorder(lines) #guardamos en la orden las lineas que teniamos anteriormente y luego las limpiamos
                 i+=1
                 line=(f.readline())
-                orders.append(Order(line))#El primer elemento del archivo es el orden del pedido. lo guardamos aparte
+                orders.append(Order(line, init_pos))#El primer elemento del archivo es el orden del pedido. lo guardamos aparte
                 lines=[]
             else:
                 line=f.readline()
@@ -26,6 +26,8 @@ def orders(): #Funcion para leer el archivo de ordenes y separar cada orden
                     lines.append(line)
         #for j in range(i):
             #orders[j].getorder()
+        orders_length = len(orders)
+        orders.remove(orders[orders_length-1]) #al final se agregaba una orden sin nada, en vez de arreglarlo la quito asi y a la mierda
     return orders
 
 
@@ -35,12 +37,22 @@ if __name__ == "__main__":
     rows=13
     dx=3
     dy=6
-    n=10
     warehouse = Warehouse(rows,columns, dx, dy)
     aisles=warehouse.create_aisles()
     shelves=warehouse.create_shelves()
-    #for i in range(len(shelves)): #En la estanteria le ponemos el valor como lo tenemos en ordenes
-    #    shelves[i]= 'P'+str(i)
-    orders=orders()
+    init_pos = [0,0]
+    orders=orders(init_pos)
+    
+    temp_ini_annealing = 20
+    temp_fin_annealing = 0.1
+    alpha_annealing = 0.5
+    
+    population_length = 10
+    it = 5
+    max_time_in_hours = 5
+    tolerance = 1
+    genetic_algorithm = Gen(population_length, warehouse, orders, temp_ini_annealing, temp_fin_annealing, alpha_annealing)
+    genetic_algorithm.population_init()
+    genetic_algorithm.GA(it, max_time_in_hours/3600, tolerance)
 
     
