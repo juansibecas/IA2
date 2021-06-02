014efeed8eb8074f4c1484f8d6e464dd0a0d11e3
Facundo Cárdenas <facu26giol@gmail.com>
2021-06-02 12:49:27 -0300

    falta backtracking

--- TP1_5/CSP.py
+++ TP1_5/CSP.py
@@ -10,33 +10,12 @@ class Graph:
     _ assignment: Lista con la Tarea su maquina y hora de inicio
     _ csp_act: Copiamos el grafo de restricciones actual para luego hacer el backtracking en caso de ser necesario
     """
-    def __init__(self,total_time):
-        self.task={}
-        self.domain = {}
-        self.neighbors = {}
-        self.assignment = []
-        self.csp_act=[]
+    def __init__(self,total_time,domain,neighbors,time_task,assignment):
+        self.time_task=time_task
+        self.domain = domain
+        self.neighbors = neighbors
+        self.assignment = assignment
         self.total_time=total_time
-
-    def set_graph(self,task,machine):
-        # Asignamos los valores del dominio y vecinos de cada tarea.
-    
-        for i in task:              
-            listneighbors=[]    #listas vacias para ir guardando los valores de los vecinos y dominio
-            listdomain=[]
-            for j in machine:
-                if i[2]==j[1]:
-                    for x in range(self.total_time-i[1]):  #Tenemos el ultimo valor al que puede tomar para no pasarle del tiempo total de produccion. en i[1] esta la duracion de la tarea
-                        listdomain.append((j[0],x)) #Guardamos que maquina puede usar y los valores de inicio posibles
-            for k in task: #Volvemos a recorrer tdas las tareas y verificamos con cuales otra tarea tiene restricciones para crear los vecinos.
-                if i != k:
-                    if i[2]==k[2]:
-                        listneighbors.append("T"+str(k[0]))
-            self.neighbors.setdefault("T"+str(i[0]),listneighbors) #guardamos los valores en los diccionarios
-            self.domain.setdefault("T"+str(i[0]),listdomain)
-            self.task.setdefault("T"+str(i[0]),i[1])
-        self.csp_act.append((self.assignment))
-        #print(self.csp_act)
 
     def heuristic_MRV(self):
         #la siguiente variable a asignar será aquella que tenga menos valores posibles en su dominio
@@ -64,27 +43,23 @@ class Graph:
                     nd=ndomain
         return variable
 
-    def inference(self,variable):
-        self.domain.pop(variable[0])
-        for x in range(0,variable[2]+self.task[variable[0]]):
+    def inference(self,variable,domain,neighbors,assignments):
+        domain.pop(variable[0])
+        for x in range(variable[2]+self.time_task[variable[0]]):
             value=(variable[1],x)
-            for var in self.domain:
+            for var in domain:
                 flag_delete=0
-                for val in self.domain[var]:
+                for val in domain[var]:
                     if val[0]==variable[1]:
                         if flag_delete==0:
-                            self.domain[var].remove(value)
-                            flag_delete=1          
-        self.assignment.append(variable)
-        self.csp_act.append((self.assignment))
-        self.search()
-
-    def search(self):
-        if not self.domain:
-            return print(self.assignment)
-        variable=self.heuristic_MRV()
-        self.inference(variable)
-            
-
-    
-
+                            domain[var].remove(value)
+                            flag_delete=1
+        neighbors.pop(variable[0])
+        for varn in neighbors: 
+            if variable[0] in neighbors[varn]:
+                neighbors[varn].remove(variable[0])          
+        assignments.append(variable)
+        self.domain=domain
+        self.neighbors=neighbors
+        self.assignment=assignments
+        return domain,neighbors,assignments
--- TP1_5/TP1_5.py
+++ TP1_5/TP1_5.py
@@ -1,9 +1,42 @@
 from CSP import Graph
+def create_disc(task,machine,total_time):
+    # Asignamos los valores del dominio y vecinos de cada tarea.
+    neighbors={}
+    domain={}
+    time_task={}
+    for i in task:              
+        listneighbors=[]    #listas vacias para ir guardando los valores de los vecinos y dominio
+        listdomain=[]
+        for j in machine:
+            if i[2]==j[1]:
+                for x in range(total_time-i[1]):  #Tenemos el ultimo valor al que puede tomar para no pasarle del tiempo total de produccion. en i[1] esta la duracion de la tarea
+                    listdomain.append((j[0],x)) #Guardamos que maquina puede usar y los valores de inicio posibles
+        for k in task: #Volvemos a recorrer tdas las tareas y verificamos con cuales otra tarea tiene restricciones para crear los vecinos.
+            if i != k:
+                if i[2]==k[2]:
+                    listneighbors.append("T"+str(k[0]))
+        neighbors.setdefault("T"+str(i[0]),listneighbors) #guardamos los valores en los diccionarios
+        domain.setdefault("T"+str(i[0]),listdomain)
+        time_task.setdefault("T"+str(i[0]),i[1])
+    return domain,neighbors,time_task
+
+def csp(total_time,domain,neighbors,time_task,assignments,index):
+    if not domain:
+        return print(assignments)
+    graph.append(Graph(total_time,domain,neighbors,time_task,assignments))
+    variable=graph[index].heuristic_MRV()
+    domain,neighbors,assignments=graph[index].inference(variable,domain,neighbors,assignments)
+    index=index+1
+    csp(total_time,domain,neighbors,time_task,assignments,index)
+
 
 if __name__ == '__main__':
     task=[[1,10,1], [2,5,1],[3,10,1],[4,15,1],[5,3,2],[6,20,2]] #Tareas de la forma: [ID, Duracion, Tipo de maquina]
     machine=[[1,1],[2,1],[3,2]] #Maquinas de la forma: [ID,Tipo de maquina]
     total_time=24               #Tomamos una produccion continua de 24 hs                    
-    graph=Graph(total_time)           
-    graph.set_graph(task,machine)    #Creamos el grafo de restrcciones
-    graph.search()
+    domain,neighbors,time_task=create_disc(task,machine,total_time)
+    assignments=[]
+
+    graph=[]
+    graph.append(Graph(total_time,domain,neighbors,time_task,assignments)) #Creamos el grafo de restrcciones inicial          
+    result=csp(total_time,domain,neighbors,time_task,assignments,1)
