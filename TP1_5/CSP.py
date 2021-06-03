fb314ff048e5d0864f8caf8dc8abfbcf722d936f
Facundo Cárdenas <facu26giol@gmail.com>
2021-06-02 21:05:35 -0300

    falta probar si funciona backtracking

--- TP1_5/CSP.py
+++ TP1_5/CSP.py
@@ -17,49 +17,52 @@ class Graph:
         self.assignment = assignment
         self.total_time=total_time
 
-    def heuristic_MRV(self):
-        #la siguiente variable a asignar será aquella que tenga menos valores posibles en su dominio
+    def heuristic_MRV(self,not_use):
+        #la siguiente variable a asignar será aquella que tenga menos valores posibles en su dominio, pero no las q estan en not_use como backtracking
         variable=['T0',0,0] #esta definida con (tarea,numero de maquina, hora inicio)
-        nd= self.total_time+1 
+        nd= self.total_time+1
+        flag_allerror=1 
         for var in self.domain:       #recorremos las tareas en el diccionario, que es una lista de tuplas de 2 elemtos
-            nmachine=self.domain[var][0][0]     #Numero de la maquina para luego comparar.
-            list_domain=[]
-            for value in self.domain[var]:
-                if value[0]==nmachine:           #Para separar las tareas que pueden usar dos maquinas
-                    list_domain.append(value)
-                    ndomain=len(list_domain)
-                else:                               
-                    if ndomain < nd:    #guardamos los valores en las variables
+            if self.domain[var] not in not_use:
+                flag_allerror=0
+                nmachine=self.domain[var][0][0]     #Numero de la maquina para luego comparar.
+                list_domain=[]
+                for value in self.domain[var]:      
+                    if value[0]==nmachine:           #Para separar las tareas que pueden usar dos maquinas
+                        list_domain.append(value)
+                        ndomain=len(list_domain)
+                    else:                               
+                        if ndomain < nd:    #guardamos los valores en las variables
+                            timeini=list_domain[0][1]
+                            variable=[var,nmachine,timeini]
+                            ndomain=nd
+                        list_domain=[]
+                        nmachine=value[0]           #Para separar las tareas que pueden usar dos maquinas
+                        list_domain.append(value)
+                        ndomain=len(list_domain)
+                if ndomain < nd:
                         timeini=list_domain[0][1]
                         variable=[var,nmachine,timeini]
-                        ndomain=nd
-                    list_domain=[]
-                    nmachine=value[0]           #Para separar las tareas que pueden usar dos maquinas
-                    list_domain.append(value)
-                    ndomain=len(list_domain)
-            if ndomain < nd:
-                    timeini=list_domain[0][1]
-                    variable=[var,nmachine,timeini]
-                    nd=ndomain
-        return variable
+                        nd=ndomain
+        return variable,flag_allerror
 
-    def inference(self,variable,domain,neighbors,assignments):
-        domain.pop(variable[0])
-        for x in range(variable[2]+self.time_task[variable[0]]):
-            value=(variable[1],x)
-            for var in domain:
+    def inference(self,variable): #variable :(tarea,numero de maquina, hora inicio)
+        flag_error=0
+        self.domain.pop(variable[0])    #Borramos la variable del conjunto
+        for x in range(variable[2],variable[2]+self.time_task[variable[0]]):#Iteramos desde el tiempo de inicio de cada variable hasta el tiempo final
+            value=(variable[1],x)   #para borrar los valores del dominio que uso esa tarea en la maquina
+            for var in self.neighbors[variable[0]]: #iteramos en las variables que tiene resticciones
                 flag_delete=0
-                for val in domain[var]:
-                    if val[0]==variable[1]:
-                        if flag_delete==0:
-                            domain[var].remove(value)
-                            flag_delete=1
-        neighbors.pop(variable[0])
-        for varn in neighbors: 
-            if variable[0] in neighbors[varn]:
-                neighbors[varn].remove(variable[0])          
-        assignments.append(variable)
-        self.domain=domain
-        self.neighbors=neighbors
-        self.assignment=assignments
-        return domain,neighbors,assignments
+                for val in self.domain[var]:    #iteramos todos los valores de cada dominio para borralos        
+                    if val[0]==variable[1]:     #para ver si usan la misma maquina y borrarlo, pq algunas tareas pueden usar distintas maquinas      
+                        self.domain[var].remove(value)
+                        break   #solo tenemos que borrar un valor del dominio a la vez
+                if self.domain[var]==[]:
+                    flag_error=1
+        self.neighbors.pop(variable[0])     #Borramos la variable de las reestricciones
+        for varn in self.neighbors:         #Borramos las reestricciones de esa variable en las demas      
+            if variable[0] in self.neighbors[varn]:
+                self.neighbors[varn].remove(variable[0])          
+        self.assignment.append(variable)    #asigamos la variable
+        return self.domain,self.neighbors,self.assignment,flag_error
+
--- TP1_5/TP1_5.py
+++ TP1_5/TP1_5.py
@@ -1,4 +1,5 @@
 from CSP import Graph
+import copy
 def create_disc(task,machine,total_time):
     # Asignamos los valores del dominio y vecinos de cada tarea.
     neighbors={}
@@ -20,23 +21,41 @@ def create_disc(task,machine,total_time):
         time_task.setdefault("T"+str(i[0]),i[1])
     return domain,neighbors,time_task
 
-def csp(total_time,domain,neighbors,time_task,assignments,index):
-    if not domain:
-        return print(assignments)
-    graph.append(Graph(total_time,domain,neighbors,time_task,assignments))
-    variable=graph[index].heuristic_MRV()
-    domain,neighbors,assignments=graph[index].inference(variable,domain,neighbors,assignments)
+def csp(index,graph,total_time,domain,neighbors,time_task,assignment,list_not_use):
+    if not graph[index].domain:     #Cuando esta vacio quiere decir que termino de asignar todas las tareas
+        return print(graph[index].assignment)
+    graph.append(Graph(total_time,copy.deepcopy(domain),copy.deepcopy(neighbors),time_task,copy.deepcopy(assignment))) #Creamos grafos de reestriciones en cada iteracion por si queremos hacer backtracking         
     index=index+1
-    csp(total_time,domain,neighbors,time_task,assignments,index)
-
+    variable,flag_allerror=graph[index].heuristic_MRV(list_not_use[index-1]) #Elegimos la variable con menor dominios para elegir (heuristica)
+    if flag_allerror==1:    #Si todas las variables de ese arbol no llegan a ningun resultado tenemos que volver al nodo anterior
+        list_not_use[index].append(graph[index].assignment[index]) #agregamos la asignacion usada que no llevaba a ninguna solucion para no usarla
+        graph[index].pop()      #borramos el ultimo grafo
+        index=index-1 
+        list_not_use[index+1].pop() #borramos estas variables para volver al nodo anterior del arbol
+        domain=graph[index].domain
+        neighbors=graph[index].neighbors
+        assignment=graph[index].assignment
+    else:    
+        domain,neighbors,assignment,flag_error=graph[index].inference(variable) #Borramos la variable eleiga y sus reestriciones con las demas y podamos el arbol
+        if flag_error==1:      #Si esa variable variable da a una solucion no valida hacemos backtracking
+            graph[index].pop()  #borramos el grafo ultimo
+            index=index-1
+            domain=graph[index].domain
+            neighbors=graph[index].neighbors
+            assignment=graph[index].assignment
+            list_not_use[index].append(variable)          #variables para no usar 
+        else:
+            list_not_use.append([]) 
+    csp(index,graph,total_time,domain,neighbors,time_task,assignment,list_not_use) #Iteramos
 
 if __name__ == '__main__':
-    task=[[1,10,1], [2,5,1],[3,10,1],[4,15,1],[5,3,2],[6,20,2]] #Tareas de la forma: [ID, Duracion, Tipo de maquina]
+    task=[[1,10,1], [2,5,1],[3,10,1],[4,15,1],[5,3,2],[6,5,2],[7,10,2]] #Tareas de la forma: [ID, Duracion, Tipo de maquina]
     machine=[[1,1],[2,1],[3,2]] #Maquinas de la forma: [ID,Tipo de maquina]
     total_time=24               #Tomamos una produccion continua de 24 hs                    
     domain,neighbors,time_task=create_disc(task,machine,total_time)
-    assignments=[]
-
+    assignment=[]
     graph=[]
-    graph.append(Graph(total_time,domain,neighbors,time_task,assignments)) #Creamos el grafo de restrcciones inicial          
-    result=csp(total_time,domain,neighbors,time_task,assignments,1)
+    list_not_use=[]     #hacemos una lista para las variables q no dan ningun resultado, y vamos guardando cada variable en cada nodo del arbol
+    list_not_use.append([])
+    graph.append(Graph(total_time,copy.deepcopy(domain),copy.deepcopy(neighbors),time_task,copy.deepcopy(assignment))) #Creamos el grafo de restrcciones inicial          
+    result=csp(0,graph,total_time,domain,neighbors,time_task,assignment,list_not_use) #Satisfaccion de reestricciones
